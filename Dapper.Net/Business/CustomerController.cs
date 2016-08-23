9a6cdc3f6c4adff8fbb676ba126d6527992d8121
theWornOut <ekremalpayy@gmail.com>
2016-08-23 10:22:48 +0300

    CustomerController

---
+++ Dapper.Net/Business/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using Dapper.Net.Models;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Dapper.Net.Business
+{
+    class CustomerController
+    {
+        SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDatabases"].ConnectionString);
+
+        public IEnumerable<Customers> GetCustomersList()
+        {
+            IEnumerable<Customers> ordersList = _connection.Query<Customers>("select * from customers");
+            return ordersList;
+        }
+
+        public Customers FindCustomer(object id)
+        {
+            return _connection.Query<Customers>("select * from customers where CustomerID = '" + id + "'").FirstOrDefault();
+        }
+
+        public void InsertCustomer(Customers customer)
+        {
+            _connection.Execute("insert into customers(CustomerID, CompanyName,ContactName,ContactTitle) VALUES (@CustomerID, @CompanyName, @ContactName, @ContactTitle); select * from customers", customer);
+        }
+
+        public void UpdateCustomer(Customers customer)
+        {
+            _connection.Execute("update customers set CustomerID = '" + customer.CustomerID + "', CompanyName = '" + customer.CompanyName + "', ContactName= '" + customer.ContactName + "', ContactTitle = '" + customer.ContactTitle + "' where CustomerID = '" + customer.CustomerID + "'");
+        }
+
+        public void DeleteCustomer(Customers customer)
+        {
+            _connection.Execute("delete from customers where CustomerID = '" + customer.CustomerID + "'");
+        }
+    }
+}
--- Dapper.Net/Main.cs
+++ Dapper.Net/Main.cs
@@ -1,54 +1,16 @@
 ﻿using Dapper.Net.Models;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Windows.Forms;
-using System.Linq;
+using Dapper.Net.Business;
 
-//Değiştirilecek. Dinamik yapı kazandırılmalı.
 namespace Dapper.Net
 {
     public partial class Main : Form
     {
-        string sConn = "";
+        CustomerController customerController = new CustomerController();
         public Main()
         {
             InitializeComponent();
-            sConn = ConfigurationManager.ConnectionStrings["MyDatabases"].ConnectionString;
-        }
-
-        private IEnumerable<Customers> GetCustomersList()
-        {
-            using (var connection = new SqlConnection(sConn))
-            {
-                IEnumerable<Customers> ordersList = connection.Query<Customers>("select * from customers");
-                return ordersList;
-            }
-        }
-
-        private void InsertCustomer(Customers customer)
-        {
-            using (var connection = new SqlConnection(sConn))
-            {
-                connection.Execute("insert into customers(CustomerID, CompanyName,ContactName,ContactTitle) VALUES (@CustomerID, @CompanyName, @ContactName, @ContactTitle); select * from customers", customer);
-            }
-        }
-
-        private void UpdateCustomer(Customers customer)
-        {
-            using (var connection = new SqlConnection(sConn))
-            {
-                connection.Execute("update customers set CustomerID = '" + customer.CustomerID + "', CompanyName = '" + customer.CompanyName + "', ContactName= '" + customer.ContactName + "', ContactTitle = '" + customer.ContactTitle + "' where CustomerID = '" + customer.CustomerID + "'");
-            }
-        }
-
-        private void DeleteCustomer(Customers customer)
-        {
-            using (var connection = new SqlConnection(sConn))
-            {
-                connection.Execute("delete from customers where CustomerID = '" + customer.CustomerID + "'");
-            }
         }
 
         private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
@@ -64,7 +26,7 @@ namespace Dapper.Net
 
         private void btnList_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = GetCustomersList();
+            dataGridView1.DataSource = customerController.GetCustomersList();
         }
 
         private void btnInsert_Click(object sender, EventArgs e)
@@ -75,31 +37,23 @@ namespace Dapper.Net
             c.ContactName = textBox3.Text;
             c.ContactTitle = textBox4.Text;
 
-            InsertCustomer(c);
+            customerController.InsertCustomer(c);
+            customerController.GetCustomersList();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            using (var connection = new SqlConnection(sConn))
-            {
-                var c = connection.Query<Customers>("select * from customers where CustomerID = '" + textBox1.Text + "'").FirstOrDefault();
-                c.CustomerID = textBox1.Text;
-                c.CompanyName = textBox2.Text;
-                c.ContactName = textBox3.Text;
-                c.ContactTitle = textBox4.Text;
+            Customers c = customerController.FindCustomer(textBox1.Text);
+            customerController.UpdateCustomer(c);
+            customerController.GetCustomersList();
 
-                UpdateCustomer(c);
-            }
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            using (var connection = new SqlConnection(sConn))
-            {
-                var c = connection.Query<Customers>("select * from customers where CustomerID = '" + textBox1.Text + "'").FirstOrDefault();
-
-                DeleteCustomer(c);
-            }
+            Customers c = customerController.FindCustomer(textBox1.Text);
+            customerController.DeleteCustomer(c);
+            customerController.GetCustomersList();
         }
     }
 }
